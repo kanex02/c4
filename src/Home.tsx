1dfa2c62c5c339d70cd80bd33403f3a810724891
kanex02 <kanexie04@gmail.com>
2023-11-25 17:05:53 +1300

    Icon install and home page change

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {AppBar, Box, Button, IconButton, Switch, Toolbar, Typography} from "@mui/material";
+import {Menu} from "@mui/icons-material";
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 function Home() {
   return (
     <div className="App">
-      <h1>HIIIIIIII</h1>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     </div>
   );
 }
